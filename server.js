d4c4b3fde096068c406bf3bf062ba42b00bd56a5
Svetlana Nikonova-Plevko <svetlananikpl@gmail.com>
2016-02-22 16:14:37 +0300

    reading json

--- server.js
+++ server.js
@@ -2,13 +2,28 @@
  * Created by Admin on 22.02.2016.
  */
 
-var express = require('express'),
-    app = express();
+var express = require('express');
+//var cors = require('cors');
+//var bodyParser = require('body-parser')
+var app = express();
 
-app.get('', function (req, res) {
-    res.send('Hello World!');
+
+//app.use(bodyParser());
+//app.use(cors());
+
+
+var fs = require('fs');
+
+fs.readFile('data/complex.json', function (err, logData) {
+
+    if (err) throw err;
+
+    console.log(logData.toString());
 });
+/*app.get('data/complex.json', function (req, res) {
+ res.send();
+ });*/
 
 app.listen(8080, function () {
-    console.log('Example');
+    //console.log('hello');
 });
