3b980a51c08d5d48cf2db584edf80c8688835ceb
Svetlana Nikonova-Plevko <svetlananikpl@gmail.com>
2016-03-01 12:44:24 +0300

    add vialotationDateInMs with generateDate.js

--- data/generateDate.js
+++ data/generateDate.js
@@ -6,17 +6,16 @@ var obj = JSON.parse(fs.readFileSync('vialotaions.json', 'utf8'));
 
 obj.forEach(function (item) {
     var vialotaionTime = new Date();
-    var days = Math.random()*1000;
-    while(days > 90){
-        days = Math.random()*1000;
+    var threeMonthsInMsec = 7889238000;
+    function getRandomArbitrary(min, max) {
+        return Math.random() * (max - min) + min;
     }
-    vialotaionTime.setDate(vialotaionTime.getDate() - days);
-    vialotaionTime.setDate(vialotaionTime.getDate() - days);
+    vialotaionTime.setTime(vialotaionTime.getTime() - getRandomArbitrary(0,threeMonthsInMsec));
     item.vialotaionTime = vialotaionTime;
 });
 
-fs.writeFile("vialotaions.json", JSON.stringify(obj, null, 3), function(err) {
-    if(err) {
+fs.writeFile("vialotaions.json", JSON.stringify(obj, null, 3), function (err) {
+    if (err) {
         return console.log(err);
     }
 
