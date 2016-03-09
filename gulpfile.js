49ce6ae3edc8a1a89945d8f7f7d9a80fe5cb302a
Svetlana Nikonova-Plevko <svetlananikpl@gmail.com>
2016-03-09 11:52:45 +0300

    update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@
 var gulp = require('gulp');
 var sass = require("gulp-sass");
 
-gulp.task('default', function () {
+gulp.task('sass', function () {
     gulp.src('./public/app/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./public/app/css'));
