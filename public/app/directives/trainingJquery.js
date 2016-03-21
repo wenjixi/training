28ab2fd7668003ad2768bdc61269b020ffaf8dbc
Svetlana Nikonova-Plevko <svetlananikpl@gmail.com>
2016-03-21 16:25:28 +0300

    add new tamplate in trainingJquery.js and add Jquery for this

--- public/app/directives/trainingJquery.js
+++ public/app/directives/trainingJquery.js
@@ -5,8 +5,9 @@ angular.module('appTrainingJquery', [])
     .directive('trainingJquery', function () {
         return {
             link: function (scope, element, attrs) {
-                $('div > h1').addClass('green')
+                $('div > h1').addClass('green');
+                $("input[name='lname']").val('Family')
             },
-            template: "<form><h1>abc</h1><div><h1>div-1</h1><h1>div-2</h1></div><h1>xyz</h1></form>"
+            template: "<form><h1>abc</h1><div><h1>div-1</h1><h1>div-2</h1></div><h1>xyz</h1></form><form  name='demo_form'>First name: <input type='text' name='fname'><br>Last name: <input type='text' name='lname'><br><input type='submit' value='Submit'></form>"
         }
     });
